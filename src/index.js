2ef657b7932c576aebaba189b19c1b10a0de9d02
Mario Brito <mariobrito94@hotmail.com>
2022-03-01 13:50:39 -0500

    Added functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+// const newtoDo = document.querySelector('#new-task-description')
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log("The DOM has loaded.");
+  document.querySelector('form').addEventListener('submit', (s) => {
+    s.preventDefault();
+    addingToDo(s.target['new-task-description'].value)
+  })
 });
+function addingToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', btndelete)
+  btn.textContent = ' X '
+  p.textContent = `${todo}   `
+  p.appendChild(btn)
+  document.querySelector('#tasks').append(p);
+}
+
+function btndelete(e){
+  e.target.parentNode.remove()
+}
+
+// let newToDo = document.querySelector('#new-task-description')
